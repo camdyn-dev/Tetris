9169fcff7c4f486483b0fd9c981b9e75685d3ffa
camdyn <camdyn.dev@tutanota.com>
2024-06-20 12:57:08 -0700

    Implement more game functionality (the game is starting to work!)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,7 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Tetris.Blocks
+using Tetris.Blocks;
 namespace Tetris
 {
     /// <summary>
@@ -78,7 +78,7 @@ namespace Tetris
 
         private void DrawGrid(GameGrid grid)
         {
-            for (int row = 0; row  <= grid.Rows; row++)
+            for (int row = 0; row < grid.Rows; row++)
             {
                 for (int column = 0; column < grid.Columns; column++)
                 {
@@ -102,15 +102,79 @@ namespace Tetris
             DrawBlock(gameState.CurrentBlock);
         }
 
+        private async Task GameLoop()
+        {
+            Draw(_gameState); // Draw the game
+
+            while (!_gameState.GameOver)
+            {
+                await Task.Delay(500); // This is the delay between the block going down automatically
+                _gameState.MoveBlockDown();
+                Draw(_gameState);
+            }
+
+            GameOverMenu.Visibility = Visibility.Visible;
+        }
+
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
+            if(_gameState.GameOver)
+            {
+                return; // We don't want anything to happen if the game is over
+            }
 
+            switch (e.Key)
+            {
+                // Left movement
+                case Key.Left:
+                case Key.A:
+                    _gameState.MoveBlockLeft();
+                    break;
+                
+                // Right movement
+                case Key.Right:
+                case Key.D:
+                    _gameState.MoveBlockRight();
+                    break;
+
+                // Down movement
+                case Key.Down:
+                case Key.S:
+                    _gameState.MoveBlockDown();
+                    break;
+
+                // Clockwise rotation
+                case Key.Up:
+                case Key.W:
+                case Key.E:
+                    _gameState.RotateBlockClockwise();
+                    break;
+
+                // Counterclockwise rotation
+                case Key.Z:
+                case Key.Q:
+                    _gameState.RotateBlockCounterClockwise();
+                    break;
+
+                default:
+                    return;
+           
+            }
+
+            Draw(_gameState);
         }
 
-        private void GameCanvas_Loaded(object sender, RoutedEventArgs e)
+        private async void GameCanvas_Loaded(object sender, RoutedEventArgs e)
         {
-            Draw(_gameState);
+            await GameLoop();
+        }
+
+        private async void PlayAgain_Click(object sender, RoutedEventArgs e)
+        {
+            _gameState  = new GameState();
+            GameOverMenu.Visibility = Visibility.Hidden;
+            await GameLoop();
         }
     }
 }
