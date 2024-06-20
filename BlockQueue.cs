4f78ac4a9393ae0823c88db422ac6697b2ffcd52
camdyn <camdyn.dev@tutanota.com>
2024-06-19 21:50:17 -0700

    Implement BlockQueue

---
+++ BlockQueue.cs
@@ -0,0 +1,48 @@
+﻿using Tetris.Blocks;
+
+namespace Tetris
+{
+    public class BlockQueue // This will let us put new blocks into the queue, dropping them into the grid
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        private readonly Random random = new Random(); // Random generator to randomly pick a number for the block
+
+        public Block NextBlock { get; private set; } // This will show what the next block is, so we know what to expect
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        private Block RandomBlock() // This will return a random block
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate() // This is to make sure we don't get the same block two times in a row
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            }
+            while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+
+    }
+
+    
+}
